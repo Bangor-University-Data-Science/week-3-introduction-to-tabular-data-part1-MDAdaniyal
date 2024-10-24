716b2bb84f0487d031d9b5010c01fe54df526716
MDAdaniyal <158162488+MDAdaniyal@users.noreply.github.com>
2024-10-24 22:42:11 +0100

    .

--- titanic_analysis/numerical_df.py
+++ titanic_analysis/numerical_df.py
@@ -1,13 +1,2 @@
-import pandas as pd
 def get_numerical_df(df, numerical_features):
-    """
-    Creates a DataFrame containing only numerical features.
-    
-    Args:
-        df (pd.DataFrame): The Titanic dataset as a DataFrame.
-        numerical_features (list): List of numerical feature names.
-    
-    Returns:
-        pd.DataFrame: DataFrame containing only numerical features.
-    """
-    return df[df[numerical_features]]
+    return df[df.select_dtypes(exclude=['object']).columns.to_list()]
--- titanic_analysis/summary_table.py
+++ titanic_analysis/summary_table.py
@@ -1,20 +1,11 @@
 import pandas as pd
 def create_summary_table(df):
-    """
-    Creates a summary DataFrame with feature name, data type, number of unique values, and if it has missing values.
-    
-    Args:
-        df (pd.DataFrame): The Titanic dataset as a DataFrame.
-    
-    Returns:
-        pd.DataFrame: A summary DataFrame.
-    """
-    titanic_df = pd.read_csv('titanic.csv')
-
-    summary = pd.DataFrame({
-    'Feature Name': titanic_df.columns,
-    'Data Type': titanic_df.dtypes.values,
-    'Number of Unique Values': titanic_df.nunique().values,
-    'Has Missing Values?': titanic_df.isnull().any().values
-     })
-    return summary
+    summary_data = {
+        'Feature Name': df.columns,
+        'Data Type': df.dtypes.values,
+        'Has Missing Values?': df.isnull().any().values,
+        'Number of Unique Values': df.nunique().values
+    }
+   
+    summary_df = pd.DataFrame(summary_data)
+    return summary_df
